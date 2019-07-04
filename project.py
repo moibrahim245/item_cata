0d7c8ec8413909528e39ad0470c6384f70efd69f
mohamed <moibrahim245@gmail.com>
2019-07-04 21:16:23 +0200

    change database

--- project.py
+++ project.py
@@ -23,7 +23,7 @@ APPLICATION_NAME = "Restaurant Menu Application"
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('postgresql://catalog:password@localhost/catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
